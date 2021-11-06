abf3255dfe86f15a8a1258d64893f02c09d88743
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-05 21:45:56 -0400

    class-based OOP application in banking

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    this.transactions.forEach(elm => {
+      balance += elm;
+    })
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      // Keep track of the time of the transaction
+      this.time = new Date();
+      // Add the transaction to the account
+      this.account.addTransaction(this.value);
+      return true;
+    }
+    return false;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance + this.amount >= 0);
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("myacc");
+console.log('Starting Balance:', myAccount.balance);
+
+t1 = new Deposit(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance);
+
+console.log('Ending Balance:', myAccount.balance);
